265a81a6b08c235a73aec7efc602098706260d55
Dibya Darshini Mahalik <dibyadarshini031@gmail.com>
2025-11-25 23:16:53 +0530

    update function.js

---
+++ function.js
@@ -0,0 +1,13 @@
+//sum function
+function sum(a, b) {
+    return a + b;
+}
+
+const arrowsum = (a, b) => {
+    console.log(a + b);
+};
+
+//multiplication function
+function mul(a, b) {
+    return a * b
+}
