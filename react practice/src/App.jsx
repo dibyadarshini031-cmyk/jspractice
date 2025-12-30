b0b2d09c6b0d61dd932d0c1042b4af23f0459c7b
Dibya Darshini Mahalik <dibyadarshini031@gmail.com>
2025-12-30 15:06:31 +0530

    react files

--- react practice/src/App.jsx
+++ react practice/src/App.jsx
@@ -111,20 +111,43 @@
 
 // export default App
 
-import { UseState } from "react";
+// import { UseState } from "react";
 
-const Counter = () => {
-    const [count, setCount] = UseState(0);
-    const [rCounter,setRCounter]= UseState(10)
+// const Counter = () => {
+//     const [count, setCount] = UseState(0);
+//     const [rCounter,setRCounter]= UseState(10)
 
+//     return (
+//         <div>
+//             <h1>Counter:{count}</h1>
+//             <h2>R counter: {rCounter}</h2>
+//             <button onClick={() => setCount(count + 1)}>Update Counter</button>
+//             <button onClick={() => setRCounter(rcounter - 1)}>Update  R Counter</button>
+//         </div>
+//     )
+// }
+
+// export default Counter;
+
+
+
+import { useState } from "react"
+function App() {
+    const [count,setCount]=useState(0);
     return (
         <div>
-            <h1>Counter:{count}</h1>
-            <h2>R counter: {rCounter}</h2>
-            <button onClick={() => setCount(count + 1)}>Update Counter</button>
-            <button onClick={() => setRCounter(rcounter - 1)}>Update  R Counter</button>
+            <h1>{count}</h1>
+            <button onClick={()=>setCount(count+1)}>Counter</button>
+            {
+                count==0?<h1>Condition 0</h1>
+                :count==1?<h1>Condition 1</h1>
+                :count==2?<h1>Condition 2</h1>
+                :count==3?<h1>Condition 3</h1>
+                :count==4?<h1>Condition 4</h1>
+                :<h1>Other condition</h1>
+            }
         </div>
     )
 }
 
-export default Counter;
+export default App
