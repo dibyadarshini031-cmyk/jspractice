2be78e2f1e8dfd34ac633a6b08fac1c1f3c7cd61
Dibya Darshini Mahalik <dibyadarshini031@gmail.com>
2026-01-16 20:38:39 +0530

    add files

--- react practice/src/Skills.jsx
+++ react practice/src/Skills.jsx
@@ -1,41 +1,41 @@
 
-import { useState } from "react";
+// import { useState } from "react";
 
-function Skills() {
-    const [skills,setSkills]=useState([]);
-    const handleSkills=(event)=>{
-        console.log(event.target.value, event.target.checked);
-        if(event.target.checked){
-            setSkills([...skills,event.taget.value])
-        }else{
-            setSkills([...skills.filter((item)=>item!=event.target.value)])
-        }
-    }
+// function Skills() {
+//     const [skills,setSkills]=useState([]);
+//     const handleSkills=(event)=>{
+//         console.log(event.target.value, event.target.checked);
+//         if(event.target.checked){
+//             setSkills([...skills,event.taget.value])
+//         }else{
+//             setSkills([...skills.filter((item)=>item!=event.target.value)])
+//         }
+//     }
 
-    return(
-        <div>
-            <h3>Select Your Skills</h3>
-            <input onChange={handleSkills} type="checkbox" id="php" value="php" />
-            <label htmlFor="php">PHP</label>
-            <br />
-            <br />
-            <input onChange={handleSkills} type="checkbox" id="java" value="java" />
-            <label htmlFor="java">JAVA</label>
-            <br />
-            <br />
-            <input onChange={handleSkills} type="checkbox" id="node" value="node" />
-            <label htmlFor="node">NODE</label>
-            <br />
-            <br />
-            <input onChange={handleSkills} type="checkbox" id="js" value="js"/>
-            <label htmlFor="js">JS</label>
+//     return(
+//         <div>
+//             <h3>Select Your Skills</h3>
+//             <input onChange={handleSkills} type="checkbox" id="php" value="php" />
+//             <label htmlFor="php">PHP</label>
+//             <br />
+//             <br />
+//             <input onChange={handleSkills} type="checkbox" id="java" value="java" />
+//             <label htmlFor="java">JAVA</label>
+//             <br />
+//             <br />
+//             <input onChange={handleSkills} type="checkbox" id="node" value="node" />
+//             <label htmlFor="node">NODE</label>
+//             <br />
+//             <br />
+//             <input onChange={handleSkills} type="checkbox" id="js" value="js"/>
+//             <label htmlFor="js">JS</label>
 
 
-            <h1>{skills.toString()}</h1>
-        </div>
+//             <h1>{skills.toString()}</h1>
+//         </div>
 
         
-    )
-}
+//     )
+// }
 
-export default Skills;
+// export default Skills;
--- react/Radio Button and Dropdown.jsx
+++ react/Radio Button and Dropdown.jsx
@@ -1,21 +1,4 @@
-
-// import Skills from "./Skills";
-
-// function App1() {
-//     return (
-//         <div>
-//             <h1>Handle Checkbox in React js</h1>
-//             <Skills/>
-//         </div>
-//     )
-// }
-
-// export default App1;
-
-
-
-
-
+///16.01.2025
 
 import { useState } from "react"
 
